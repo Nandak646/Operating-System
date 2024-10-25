adfb8c8228a92a1cb2dd83a9b2de7858e00c42d0
Nandak646 <nandakishorkumarreddy011@gmail.com>
2024-10-26 00:51:13 +0530

    Add files via upload

--- FCFS.c
+++ FCFS.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
-
-int main() {
-    int n;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-
-    int burst_time[n], waiting_time[n], turnaround_time[n];
-    
-    // Input burst times
-    for (int i = 0; i < n; i++) {
-        printf("Enter Burst Time for Process %d: ", i + 1);
-        scanf("%d", &burst_time[i]);
-    }
-    
-    // Calculate waiting times
-    waiting_time[0] = 0; // First process has no waiting time
-    for (int i = 1; i < n; i++) {
-        waiting_time[i] = waiting_time[i - 1] + burst_time[i - 1];
-    }
-    
-    // Calculate turnaround times
-    for (int i = 0; i < n; i++) {
-        turnaround_time[i] = waiting_time[i] + burst_time[i];
-    }
-    
-    // Print results
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\n", i + 1, burst_time[i], waiting_time[i], turnaround_time[i]);
-    }
-
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number of processes:");
+	scanf("%d",&n);
+	int bt[n],wt[n],tat[n];
+	printf("Enter thr burst time for %d processes\n",n);
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&bt[i]);
+	}
+	//calculate the waiting time
+	wt[1]=0;
+	for(int i=2;i<=n;i++)
+	{
+		wt[i]=wt[i-1]+bt[i-1];
+	}
+	//calculate the turn around time
+	for(int i=1;i<=n;i++)
+	{
+		tat[i]=bt[i]+wt[i];
+	}
+	//printing
+	for(int i=1;i<=n;i++)
+	{
+		printf("\nProcess%d	%d	%d	%d\n",i,bt[i],wt[i],tat[i]);
+	}
 }
--- SJFS.c
+++ SJFS.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
-
-void sortProcessesByBurstTime(int n, int burst_time[], int process[]) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (burst_time[j] > burst_time[j + 1]) {
-                // Swap burst times
-                int temp = burst_time[j];
-                burst_time[j] = burst_time[j + 1];
-                burst_time[j + 1] = temp;
-
-                // Swap corresponding process IDs
-                temp = process[j];
-                process[j] = process[j + 1];
-                process[j + 1] = temp;
-            }
+#include<stdio.h>
+int main()
+{
+        int n;
+        printf("Enter the number of processes:");
+        scanf("%d",&n);
+        int p[n],bt[n],wt[n],tat[n];
+        printf("Enter thr burst time for %d processes\n",n);
+        for(int i=1;i<=n;i++)
+        {
+		p[i]=i;
+	        scanf("%d",&bt[i]);
         }
-    }
-}
-
-int main() {
-    int n;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-
-    int burst_time[n], waiting_time[n], turnaround_time[n], process[n];
-
-    // Input burst times
-    for (int i = 0; i < n; i++) {
-        process[i] = i + 1; // Process IDs from 1 to n
-        printf("Enter Burst Time for Process %d: ", process[i]);
-        scanf("%d", &burst_time[i]);
-    }
-
-    // Sort processes by burst time
-    sortProcessesByBurstTime(n, burst_time, process);
-
-    // Calculate waiting times
-    waiting_time[0] = 0; // First process has no waiting time
-    for (int i = 1; i < n; i++) {
-        waiting_time[i] = waiting_time[i - 1] + burst_time[i - 1];
-    }
-
-    // Calculate turnaround times
-    for (int i = 0; i < n; i++) {
-        turnaround_time[i] = waiting_time[i] + burst_time[i];
-    }
-
-    // Print results
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\n", process[i], burst_time[i], waiting_time[i], turnaround_time[i]);
-    }
-
-    return 0;
+	//sorting
+	int temp;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i+1;j<=n;j++)
+		{
+			if(bt[i]>bt[j])
+			{
+				temp=bt[i];
+				bt[i]=bt[j];
+				bt[j]=temp;
+                                                  temp=p[i];
+				p[i]=p[j];
+				p[j]=temp;
+			}
+		}
+	}
+        //calculate the waiting time
+        wt[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+                wt[i]=wt[i-1]+bt[i-1];
+        }
+        //calculate the turn around time
+        for(int i=1;i<=n;i++)
+        {
+                tat[i]=bt[i]+wt[i];
+        }
+        //printing
+        for(int i=1;i<=n;i++)
+        {
+                printf("\nProcess%d     %d      %d      %d\n",p[i],bt[i],wt[i],tat[i]);
+        }
 }
